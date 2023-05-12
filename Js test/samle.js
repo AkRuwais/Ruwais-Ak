4b98dcdc77f1f22d2f6d6ef81ceaee49ba149b29
AkRuwais <akruwais9445@gmail.com>
2023-05-12 18:30:49 +0530

    React added

---
+++ Js test/samle.js
@@ -0,0 +1,126 @@
+// function greetings(name) {
+//   document.write("Good morning" + name);
+// }
+// greetings(" Ruwais" + "<br>");
+// greetings(" Shammas");
+
+// console.log(a);
+
+// var a;
+// console.log(a);
+
+// var a = 10;
+// console.log(x);
+
+// var a = 10;
+// var a = 100;
+// console.log(a);
+
+// function a() {
+//   for (i = 1; i <= 5; i++) {
+//     for (j = 1; j <= i; j++) {}
+//   }
+//   console.log(j);
+// }
+
+//Function-----
+// function message(){
+//   console.log("Good morning");
+// }
+// message();
+
+//Anonimusfunction-----
+// let fun1 = function () {
+//   console.log("Good morning Anonimous");
+// }
+// fun1 ();
+
+//Arrow function-----
+// let fun2 = () => console.log("Good morning Arrow");
+// fun2();
+
+// Aroow function summ-----
+// let sum = (x,y) => console.log(x+y);
+// sum(10,30);
+
+//cheking positive or negative-----
+// let Number = (num) => {
+//   if(num>0){
+//     console.log("Number is positive");
+//   } else {
+//     console.log("Number is negative");
+//   }
+// }
+// Number(10);
+
+//tebmplate literals-----
+// var name = "ruwais";
+// var age = 24;
+// console.log(`My name is ${name}. Iam ${age} Years old`);
+
+// var x = 20;
+// var y = 30;
+// console.log(`Sum of x and y is = ${x+y}`)
+
+// destructuring-----
+// const obj = {
+//   name : 'Ruwais',
+//   place : 'kottakkal',
+//   rank : 10,
+// };
+// function myFunction(){
+//   const {
+//     name,
+//     place,
+//     rank,
+//   } = obj
+//   console.log(name,place,rank);
+// }
+
+//Error handling try or catch-----
+// myFunction();
+// try {
+//   console.log(a);
+//   let a=2
+//   let b=a/0
+//   console.log(b);
+
+// } catch (error) {
+// console.log(e++) {
+
+//star pattern-----
+// var i,j;
+// for(i=1;i<=5;i++){
+//   for(j=1;j<=i;j++){
+//     document.write("*");
+//   }
+//  document.write("<br>");
+// }
+
+//piramid pattern-----
+// var i, j, k, n=5;
+// for(i = 1;i <= n;i ++){
+//   for(j = n;j >= i;j --){
+//     document.write("&nbsp&nbsp ")
+//   }
+//   for(k = 1;k <= 2*i-1;k ++){   
+//     document.write(" *")
+//   } 
+//   document.write("<br>")
+// }
+
+// document.write("<br>")
+//revers piramid-----
+// var i, j, k, n=5;
+// for(i = 1;i <= n;i ++){
+//   for(j = 1;j <= i;j ++){
+//     document.write("&nbsp&nbsp ");
+//   }
+//   for(k = 2*n+2;k >= j*2;k --){
+//     document.write(" *");
+//   }
+//   document.write("<br>");
+// }
+
+
+
